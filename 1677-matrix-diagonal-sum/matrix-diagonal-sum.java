61d4649827436e70dc3e6b9b99ef4d6adebc3db0
HARINI S <sivakkozhundhuharini@gmail.com>
2026-09-10 20:42:14 +0530

    Time: 1 ms (29.58%) | Memory: 46.1 MB (87.77%) - LeetSync

---
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.java
@@ -0,0 +1,15 @@
+ class Solution {
+    public int diagonalSum(int[][] a) {
+        int s = 0;
+        
+        for(int i = 0; i < a.length; i++) {
+            for(int j = 0; j < a[0].length; j++) {
+                if(i == j || i + j == a.length - 1) {
+                    s = s + a[i][j];
+                }
+            }
+        }
+        
+        return s;
+    }
+}
